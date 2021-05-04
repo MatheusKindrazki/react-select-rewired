440bf851fcf89538b1a21f6a096075b48b2a306b
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-05-04 18:58:09 -0300

    deploy: criando pacote

---
+++ base/dist/react-select.cjs.prod.js
@@ -0,0 +1,11 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: !0
+}), require("react"), require("memoize-one"), require("@emotion/core"), require("react-dom"), 
+require("prop-types"), require("../../dist/utils-2db2ca57.cjs.prod.js"), require("../../dist/index-4f270825.cjs.prod.js");
+
+var reactSelect = require("../../dist/Select-f398b57f.cjs.prod.js");
+
+require("@emotion/css"), require("react-input-autosize"), exports.default = reactSelect.Select, 
+exports.defaultProps = reactSelect.defaultProps;
